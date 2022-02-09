cc4b6981031295e870f9d7fe1da526c05f98804f
Abraham George Smith <abe@abesmith.co.uk>
2022-02-09 12:06:41 +0100

    updating version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (current_dir / "README.md").read_text()
 setup(
   name = 'seminal_root_angle',
   packages = ['seminal_root_angle'],
-  version = '0.0.2',
+  version = '0.0.3',
   license = 'GPLv3', 
   description = 'Extract seminal root angle from rhizobox images',
   long_description_content_type='text/markdown',
@@ -15,7 +15,7 @@ setup(
   author = 'Abraham George Smith',
   author_email = 'abe@abesmith.co.uk',
   url = 'https://github.com/Abe404/seminal_root_angle',
-  download_url = 'https://github.com/Abe404/seminal_root_angle/archive/refs/tags/0.0.2.tar.gz',
+  download_url = 'https://github.com/Abe404/seminal_root_angle/archive/refs/tags/0.0.3.tar.gz',
   keywords = ['ROOT', 'ANGLE', 'PHENOTYPING'],
   install_requires=[
       "numpy ==1.22.2",
