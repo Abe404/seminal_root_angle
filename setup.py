728e82effa3c89d3be1a32784e43603ab2c6532c
Abraham George Smith <abe@abesmith.co.uk>
2022-02-09 11:41:05 +0100

    update license identifier

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   name = 'seminal_root_angle',
   packages = ['seminal_root_angle'],
   version = '0.0.1',
-  license = 'GPL3', 
+  license = 'GPLv3', 
   description = 'Extract seminal root angle from rhizobox images',
   long_description_content_type='text/markdown',
   long_description=long_description,
@@ -19,14 +19,14 @@ setup(
   keywords = ['ROOT', 'ANGLE', 'PHENOTYPING'],
   install_requires=[
       "numpy ==1.22.2",
-      "scikit-image ==0.19.1"
-      "humanize ==3.14.0"
+      "scikit-image ==0.19.1",
+      "humanize ==3.14.0",
       "matplotlib ==3.5.1"
   ],
   classifiers=[
     'Intended Audience :: Developers',
     'Programming Language :: Python :: 3',
-    'License :: OSI Approved :: GPL3 License',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
     'Operating System :: OS Independent'
   ]
 )
